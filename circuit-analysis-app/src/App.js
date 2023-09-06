9df7c94cc7913f143c3a5ca3edf4e39b0beb58af
Najib Haidar <najibh10@uw.edu>
2023-09-05 22:11:36 -0700

    Basic click + drop text from drawer functionality

--- circuit-analysis-app/src/App.js
+++ circuit-analysis-app/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Drawer, List, ListItem, ListItemText, ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
 function App() {
+  // State for which component is currently selected
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  // State to track components placed on the canvas
+  const [componentsOnCanvas, setComponentsOnCanvas] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+
+      <Drawer
+        variant="permanent"
+        open={true}
+      >
+        <List>
+          {['Resistor', 'Voltage Source', 'Current Source', 'Wires'].map((text) => (
+            <ListItem 
+              button 
+              key={text} 
+              onClick={() => setSelectedComponent(text)}
+            >
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <div 
+        style={{ marginLeft: '240px', height: '100vh', border: '1px solid gray' }}
+        onClick={(e) => {
+          if (selectedComponent) {
+            // For now, just store the component and its position
+            setComponentsOnCanvas([
+              ...componentsOnCanvas,
+              {
+                component: selectedComponent,
+                x: e.clientX,
+                y: e.clientY
+              }
+            ]);
+
+            // Deselect the component after placing it
+            setSelectedComponent(null);
+          }
+        }}
+      >
+        {componentsOnCanvas.map((comp, index) => (
+          <div 
+            key={index} 
+            style={{ position: 'absolute', top: comp.y, left: comp.x }}
+          >
+            {comp.component}
+          </div>
+        ))}
+      </div>
+      
+    </ThemeProvider>
   );
 }
 
